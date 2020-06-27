ce7dc7280cd35759f76493c589c368107f643ac3
Anisha <anishamathais14@gmail.com>
2020-06-27 16:59:17 +0530

    ok

---
+++ src/com/luvtocode/SwimCoach.java
@@ -0,0 +1,17 @@
+package com.luvtocode;
+
+public class SwimCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Do swimming for ten min";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return "Today is your luck day";
+	}
+
+}
